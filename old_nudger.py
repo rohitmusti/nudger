dc1b0c4014e6966291bccd6647394149e0900db8
rohitmusti <rohit.musti.rm@gmail.com>
2020-02-02 13:33:06 -0500

    starting the new project

---
+++ old_nudger.py
@@ -0,0 +1,70 @@
+import subprocess
+import sys, os
+import random
+from time import sleep
+
+climate_change = ["climate", "climate change", "earth", "climate issues",
+                  "ocean rising", "nature preservation", "ocean", 
+                  "global warming", "sustainable companies",
+				  "100-year flood ",
+				  "A1B emissions scenario ","A2 emissions scenario ",
+				  "Adaptation ","Adaptation science ",
+				  "Adaptive capacity ","Adaptive management ","Aerosol (Atmospheric) ",
+				  "Algal bloom ","Assisted migration ",
+				  "B1 emissions scenario  ","B2 emissions scenario ",
+				  "Bioenergy ","Biofuel ",
+				  "Biogeochemical cycles ",
+				  "Biomass ","Black carbon ",
+				  "Carbon capture and storage ","Carbon cycle ",
+				  "Carbon sequestration ","Climate change ",
+				  "Climate change refugia ","Climate variability ",
+				  "Cold wave ","Dispersal ",
+				  "Drought ","Dry spell ",
+				  "Ecological corridor ","Ecosystem ",
+				  "Ecosystem services ","El Niño-Southern Oscillation ",
+				  "Emergent properties ","Emissions scenarios ",
+				  "Energy systems ","Environmental justice ",
+				  "Evapotranspiration ","Evolution ",
+				  "Extreme events ","Extreme precipitation (events) ",
+				  "Feedback ","Fitness ",
+				  "Food security ","Forcing ",
+				  "Frontline communities ",
+				  "Frost-free season ","Genetic rescue ",
+				  "Geoengineering ","Global change ",
+				  "Global Climate Models (GCM) ","Global warming ",
+				  "Greenhouse gases ","Heat stress ",
+				  "Heat wave ","Heavy precipitation events ",
+				  "Indicator ","Indigenous knowledges ",
+				  "Invasive species ","Land cover ",
+				  "Land use ","Life history ",
+				  "Mitigation ","Nutrients ",
+				  "Ocean acidification ","Ozone ",
+				  "Paleoclimate ","Pathogen ",
+				  "Permafrost ","Phenology ",
+				  "Phenotypic plasticity ","Phytoplankton ",
+				  "Preparedness ","Primary productivity ",
+				  "Proxies ","Radiative forcing ",
+				  "Range shift ","Representative Concentration Pathways ",
+				  "Resilience ","Resource mismatch ",
+				  "Risk ","Risk assessment ",
+				  "Risk management ","Risk perception ",
+				  "Risk-based framing ","Scenario ",
+				  "Seed sourcing ","Sink ",
+				  "Snow water equivalent (SWE) ","Snowpack ",
+				  "Special Report on Emissions Scenarios (SRES) ","Stakeholder ",
+				  "Storm surge ","Stratification ",
+				  "Stressor ","Subsistence ",
+				  "Tipping point ","Traditional knowledge ",
+				  "Transpiration ","Trophic level ",
+				  "Uncertainty ","Urban heat island effect ",
+				  "Validate ","Value ",
+				  "Value (economic) ","Vector (disease) ",
+				  "Vulnerability ","Vulnerability assessment ",
+				  "Water security ","Water stress "]
+
+with open(os.devnull, 'w') as devnull:
+	len_list = len(climate_change)
+	while 1:
+		term = climate_change[random.randrange(1, len_list)]
+		subprocess.run(["googler", "--np", term], stdout=devnull)
+		sleep(1)
